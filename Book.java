13fd18e23680e31d77b63c704cbb01a035fb48d1
eilla1 <git@ella.cx>
2021-12-13 11:38:21 -0800

    End of class

--- Book.java
+++ Book.java
@@ -1,10 +1,12 @@
 public class Book {
-    public static void books() {
-        int wordCounter = 0;
+    public static void Organize() {
+        
+        
+        /*int wordCounter = 0;
         int i = 0;
         while (words[i] != null) {
             wordCounter++;
         }
-        System.out.println("The word count is " + wordCounter);
+        System.out.println("The word count is " + wordCounter);*/
     }
 }
